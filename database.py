909109c76d1a75f1e0f5c2218bfe253c4d485613
ConfettiRaindrop <camilafigueira@outlook.com>
2024-04-25 12:58:17 -0700

    swapped pets into recipe text

--- database.py
+++ database.py
@@ -7,22 +7,23 @@ class SQL:
     mydb = mysql.connector.connect(
         host = "localhost:3306",
         user="root",
-        password="Sr1k@r36",
         database="orders"
 
     )
     cursor = mydb.cursor()
-    sql = "INSERT INTO Pets (petID, petName, kind, gender, age, ownerID, adoptionID) VALUES(R3-7551, 'Keller', 'Parrot', 'female', '2', 7908, NULL);"
-    cursor = mydb.cursor(sql)
+    #TODO: talk about adding ingredients to database -> varying amout talk as group later
+    #adds colums, and a sample recipe to database
+    sql = "INSERT INTO RecipeDatabase (title, time, servings, difficulty, ingredients, backendIngredients, quantityWhole, quantityMass, quantityVolume, steps, tags) VALUES('Sample Recipe', 10, 5, 'Advanved', 'chopped food1', 'food1', 5, NULL, NULL, 3, NULL);"
+    cursor = mydb.cursor(sql) 
 
-    def __init__(self, petID = 0, petName = 0, kind = 0, gender = 0, age = 0, ownerID = 0, adoptionID = 0) -> None:
+    def __init__(self, title = 0, time = 0, servings = 0, difficulty = 0, ingredients = 0, backendIngredients = 0, quantityWhole = 0, quantityMass = 0, quantityVolume = 0, steps = 0, tags = 0) -> None:
         #add in the database
-        sql = "USE orders; INSERT INTO Pets (petID, petName, kind, gender, age, ownerID, adoptionID) VALUES(R3-7551, 'Keller', 'Parrot', 'female', '2', 7908, NULL);"
+        sql = "USE orders; INSERT INTO RecipeDatabase (title, time, servings, difficulty, ingredients, backendIngredients, quantityWhole, quantityMass, quantityVolume, steps, tags) VALUES(" + title + ", " + time + ", " + servings +", " + difficulty + ", " + ingredients + ", " + backendIngredients + ", " + quantityWhole + ", " + quantityMass + ", " + quantityVolume + ", " + steps + ", " + tags + ");"
         
 
-    def add(self):
-        sql = "INSERT INTO Pets (petID, petName, kind, gender, age, ownerID, adoptionID) VALUES(R3-7551, 'Keller', 'Parrot', 'female', '2', 7908, NULL);"
-
+    def add(self, title = 0, time = 0, servings = 0, difficulty = 0, ingredients = 0, backendIngredients = 0, quantityWhole = 0, quantityMass = 0, quantityVolume = 0, steps = 0, tags = 0) -> None:
+        sql = "USE orders; INSERT INTO RecipeDatabase (title, time, servings, difficulty, ingredients, backendIngredients, quantityWhole, quantityMass, quantityVolume, steps, tags) VALUES(" + title + ", " + time + ", " + servings +", " + difficulty + ", " + ingredients + ", " + backendIngredients + ", " + quantityWhole + ", " + quantityMass + ", " + quantityVolume + ", " + steps + ", " + tags + ");"
+        
     def insert(self, column, value):
         pass
 
