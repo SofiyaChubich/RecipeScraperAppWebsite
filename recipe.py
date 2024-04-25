955734c55c78e4f0bf09e769a70e3a35c0adc46e
ConfettiRaindrop <camilafigueira@outlook.com>
2024-04-25 13:00:24 -0700

    added quantity holders for ingredients

--- recipe.py
+++ recipe.py
@@ -1,11 +1,14 @@
 class Recipe:
-    def __init__(self, title, time, servings, diffuculty, ingredients, steps, tags):
+    def __init__(self, title, time, servings, diffuculty, ingredients, quantityWhole, quantityMass, quantityVolume, steps, tags):
         self.title = title
         self.time = time
         self.servings = servings
         self.difficulty = diffuculty
         self.ingredients = ingredients
-        self.ingredients_backend_side = ingredients
+        self.ingredients_backend_side = ingredients #TODO: finish and call method to set backend ingredients
+        self.quantityWhole = quantityWhole
+        self.quantityMass = quantityMass
+        self.quantityVolume = quantityVolume
         self.steps = steps
         self.tags = tags
     
